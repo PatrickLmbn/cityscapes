673f7dc6b96f1afd5a7d50f704a66d6b643e97ad
Patrick Lambino <lambino.patrick.competente@gmail.com>
2026-04-20 23:29:50 +0800

    chore: update dev server configuration to use port 5111

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['cityvoid.patricklmbn.online']
+    allowedHosts: ['cityvoid.patricklmbn.online'],
+    port: 5111
   }
 })
